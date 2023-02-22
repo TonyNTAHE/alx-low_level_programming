432c1de131407916b90000dff3bc2561c38ed8ea
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-22 06:29:55 +0100

    fix bug

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -6,8 +6,19 @@
  * Return: Always 0.
  */
 void print_alphabet(void);
+int _putchar(char c);
 int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
