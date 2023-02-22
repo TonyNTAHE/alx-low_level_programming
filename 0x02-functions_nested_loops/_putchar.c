66eb0004b18db2c4bcb0b6ca93b632c74ad82295
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-22 09:19:37 +0100

    fix done

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - print the character c to stdout
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,8 @@
-#ifdef _MAIN_H
-#define _MAIN_H
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
+ 
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,5 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
