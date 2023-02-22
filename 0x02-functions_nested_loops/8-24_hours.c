1e76508305830f593af7cbb15e7f5379a376a1d3
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-22 17:56:19 +0100

    Done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; m++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf("%d0:%02d\n", h, m);
+		}
+	}
+}
