02d627aa03893cec985bd4848f6f78ce965729dd
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-23 11:43:59 +0100

    Completed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,15 +12,23 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(48);
+		for (b = 1; b <= 9 ;b++)
 		{
 			c = a * b;
-			printf("%2d", c);
-			if (b != 9)
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
 			{
-				printf(", ");
+			_putchar(32);
+			_putchar(c + 48);
+			}
+			else
+			{
+			_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 			}
 		}
-		printf("\n");
+		_putchar('\n');
 	}
-}
+}	
