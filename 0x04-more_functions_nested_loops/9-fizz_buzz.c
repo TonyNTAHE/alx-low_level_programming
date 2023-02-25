45bec63305254f009bd4f707beb00555e8117e43
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-25 19:14:14 +0100

    Completed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,23 +10,26 @@
  */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 5 == 0)
 		{
 			if (i % 3 == 0)
-				printf("FizzBuzz ");
+			{
+				printf(" FizzBuzz");
+			}
 			else
-				printf("Buzz ");
+				printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
