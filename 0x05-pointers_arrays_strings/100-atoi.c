4a9f568e47bcd3dab07637d4e7c9b374a825d873
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-02-28 15:03:15 +0100

    Completed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,17 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
+	unsigned int result = 0;
 	int sign = 1;
-	char *p = s;
 
-	if (!s)
-		return (0);
-	while (*p == ' ' || *p == '\t')
-		p++;
-	if (*p == '-' && *p == '+')
-	{
-		sign = -1;
-		p++;
-	}
-	while (*p >= '0' && *p <= '9')
-	{
-		int digit = *p - '0';
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
 
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
-		{
-			return (sign == -1 ? INT_MIN : INT_MAX);
-		}
-		result = result * 10 + digit;
-		p++;
-	}
-	return (sign * result);
+		else if (result > 0)
+		break;
+	} while (*s++);
+	return (result * sign);
 }
