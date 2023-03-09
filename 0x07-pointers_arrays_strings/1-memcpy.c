7049da453da7c81f343e0c86855f4d0cdd5843af
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-03-09 17:36:46 +0100

    Completed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; index < n; index++)
+	for (index = 0, index < n; index++)
 	{
-			dest[index] = src[index];
+		dest[index] = src[index];
 	}
 	return (dest);
 }
