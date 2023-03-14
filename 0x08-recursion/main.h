0a37068eaf85a548fc6f17e189dd412afbb2f913
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-03-14 10:42:16 +0100

    Completed

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #include <math.h>
 #include <stdio.h>
 #include <unistd.h>
