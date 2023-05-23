b331f4b49b0d68c1903c40ff025aa52f346d1d52
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-05-23 15:00:41 +0200

    fixing bug

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,27 +8,17 @@
  */
 int main()
 {
-	int i = 3;
-	int j = 5;
-	int sum1 = 0;
-	int sum2 = 0;
-	int Total_sum = 0;
+	int i;
+	int n = 1024;
+	int sum = 0;
 
-	for (i = 3; i < 1024; i++)
+	for (i = 1; i < n; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			sum1 = sum1 + i;
+			sum += i;
 		}
 	}
-	for (j = 5; j < 1024; j++)
-	{
-		if (j % 5 == 0)
-		{
-			sum2 = sum2 + j;
-		}
-	}
-	Total_sum = sum1 + sum2;
-	printf("The Total sum is: %d\n", Total_sum);
+	printf("The sum of multiples of 3 and 5 below %d is = %d\n", n, sum);
 	return (0);
 }
