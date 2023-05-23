dc6f00d3e644044c798e902bb2a3969bf463b7dd
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-05-23 15:32:30 +0200

    Done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - computes fibonacci series.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+	int r;
+	int q = 1;
+	int z = 2;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d", q);
+		if (i != 50)
+		{
+			printf(",");
+			printf(" ");
+		}
+		r = q + z;
+		q = z;
+		z = r;
+	}
+	printf("\n");
+}
