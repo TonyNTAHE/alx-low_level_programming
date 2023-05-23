30d18b3bb1c93f465acb365f5fef55b4b1bc91e5
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-05-23 15:48:09 +0200

    fixing bugs

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,10 +9,12 @@ int main(void)
 {
 	int i;
 	long int r;
-	long int q = 1;
-	long int z = 2;
-	
-	for (i = 1; i <= 50; i++)
+	long int q;
+	long int z;
+
+	q = 1;
+	z = 2;
+	for(i = 1; i <= 50; i++)
 	{
 		printf("%ld", q);
 		if (i != 50)
