723adef76801110328bfe2dd5577e12c11715fb0
TonyNTAHE <tonyntahe@gmail.com>
2023-05-31 21:00:51 +0200

    Done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,32 +5,19 @@
  * @s1: first string.
  * @s2: second string.
  * Return: 0 if both strings are equal.
- *         15 if first string is greater than second string.
- *         -15 if first string is less than second string.
+ *         (+) num if first string is greater than second string.
+ *         (-) num if first string is less than second string.
  */
 int _strcmp(char *s1, char *s2)
 {
 int i;
 int f = 0;
 
-for (i = 0; s1[i] || s2[i]; i++)
+for (i = 0; (s1[i] || s2[i]); i++)
 {
-if (s1[i] == s2[i])
-{
-f = 1;
+f = s1[i] - s2[i];
+if (f != 0)
+break;
 }
-else if (s1[i] > s2[i])
-{
-return (15);
-}
-else if (s1[i] < s2[i])
-{
-return (-15);
-}
-}
-if (f == 1)
-{
-return (0);
-}
-return (0);
+return (f);
 }
