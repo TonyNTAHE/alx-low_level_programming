bf01ee1ecbd728372b64251c6d481c9fe0c85573
TonyNTAHE <tonyntahe@gmail.com>
2023-06-05 22:43:32 +0200

    Done

---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "I am to old for this";
+    char *f = "old";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ char *_memcpy(char *dest, char *src, unsigned n);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
