6b4211974a4cf15a2b9231ba24ef9e49c2ed9bfb
TonyNTAHE <tonyntahe@gmail.com>
2023-06-13 09:31:16 +0200

    A function that returns a pointer to the allocated memory for a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns pointer to memory allocated for array of 2D.
+ * @height: rows.
+ * @width: colums.
+ * Return: NULL it fails.
+ *         NULL is rows or colums <= 0.
+ *         pointer to memory allocated to array of 2D.
+ */
+int **alloc_grid(int width, int height)
+{
+int **s;
+int i;
+
+if (width <= 0 || height <= 0)
+{
+return (0);
+}
+s = malloc(height * sizeof(int *));
+if (s == NULL)
+{
+free(s);
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+s[i] = malloc(width * sizeof(int *));
+}
+return (s);
+}
