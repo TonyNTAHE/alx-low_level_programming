ccf51c8b09e4cd2b9fe8a1ffcb0f44c9da02bcfb
TonyNTAHE <tonyntahe@gmail.com>
2023-06-19 17:49:45 +0200

    Computes the sum of the number x and y using macro

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include "4-sum.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
