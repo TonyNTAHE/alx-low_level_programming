4554318c63aa4c177ef821645e93830613da791f
TonyNTAHE <tonyntahe@gmail.com>
2023-06-19 19:49:19 +0200

    Building a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's structure
+ * @name: the name of the dog
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
