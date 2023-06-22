e5c899652f8ff383c1eeabe1bebfdc82d1cb3974
TonyNTAHE <tonyntahe@gmail.com>
2023-06-22 11:52:06 +0200

    Program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,19 +17,19 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		return (99);
-	}
 	oprt  = get_op_func(argv[2]);
 	if (oprt == NULL)
 	{
 		printf("Error\n");
-		return (100);
+		exit (99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	printf("%d\n", oprt(num1, num2));
 	return (0);
 }
