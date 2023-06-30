27312943d71058ee80e12b4178577e80b2ec7c92
TonyNTAHE <tonyntahe@gmail.com>
2023-06-30 17:24:50 +0200

    function that prints a string before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * before_main  - prints before the main function.
+ */
+void __attribute__((constructor)) tony()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
