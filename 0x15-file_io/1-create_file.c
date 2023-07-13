f301f31abaab07535fb7e439ce54c45003892036
TonyNTAHE <tonyntahe@gmail.com>
2023-07-13 15:12:35 +0200

    function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (0);
 	if (len > 0)
