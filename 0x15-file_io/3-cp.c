ff5eed716edafb5be113af48ca01396178a3b185
TonyNTAHE <tonyntahe@gmail.com>
2023-07-14 22:17:42 +0200

    Program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,6 +14,26 @@ void error_msg(int x, const char *msg, int ext_code)
 	}
 }
 /**
+ * error_file - checks if files can be opened.
+ * @f_from: file_from.
+ * @f_to: file to.
+ * @argv: argument vector.
+ * Return: no return.
+ */
+void error_file(int f_from, int f_to, char *argv[])
+{
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s'n", argv[1]);
+		exit(98);
+	}
+	if (f_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+/**
  * main - copies two files.
  * @argc: argument count.
  * @argv: argument vector.
@@ -21,24 +41,25 @@ void error_msg(int x, const char *msg, int ext_code)
  */
 int main(int argc, char **argv)
 {
-	int c, w, r;
-	int fd1, fd2;
+	ssize_t nchar, nwr;
+	int fd1, fd2, c;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "cp file_from file_to\n");
+		fprintf(stderr, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	error_msg(fd1, "Error: Can't read the file\n", 98);
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	error_msg(fd2, "Error: Can't write to file_to\n", 99);
-	while ((r = read(fd1, buffer, sizeof(buffer))) > 0)
+	error_file(fd1, fd2, argv);
+	nchar = 1024;
+	while ((nchar = read(fd1, buffer, 1024)) > 0)
 	{
-		w = write(fd2, buffer, r);
-		error_msg(w, "Error: Can't write to file_to\n", 99);
+		nwr = write(fd2, buffer, nchar);
+		error_msg(nwr, "Error: Can't write to file_to\n", 99);
 	}
+	error_msg(nchar, "Error: Can't read the file\n", 98);
 	c = close(fd1);
 	error_msg(c, "Error: Can't close fd fd1\n", 100);
 	c = close(fd2);
