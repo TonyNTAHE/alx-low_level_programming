f09bc3c93c2aa680992b8d6902061625b2587e47
TonyNTAHE <tonyntahe@gmail.com>
2023-07-15 12:19:20 +0200

    copies the content of a file to another file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	if (len > 0)
 	{
 		bytes = write(fd, text_content, len);
