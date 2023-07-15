1ae2fd347123e28295096383e78a34dc529f031b
TonyNTAHE <tonyntahe@gmail.com>
2023-07-15 11:10:48 +0200

    copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdarg.h>
+void error_msg(int x, char *msg, int ext, ...);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
