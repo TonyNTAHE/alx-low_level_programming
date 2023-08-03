3de74cf6fc35ebc2bff1028d9844efd4a0a34b9d
TonyNTAHE <tonyntahe@gmail.com>
2023-08-03 12:33:21 +0200

    fixing bugs

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,12 +8,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask;
 
 	if (index >= sizeof(unsigned long int) * 8 - 1)
 	{
 		return (-1);
 	}
-	unsigned  int mask = 1UL << index;
+	mask = 1UL << index;
 	*n &= ~(mask);
 	return (0);
 }
