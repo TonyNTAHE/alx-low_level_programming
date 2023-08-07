d028adb61e467c66ee4ccbff94d91f61c1df4518
TonyNTAHE <tonyntahe@gmail.com>
2023-08-07 14:46:31 +0200

    function that read a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX
- * standard output.
- * @filename: the file to read from.
- * @letters: is the number of letters it should read and print
- * Return: number of letters it could read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard.
+ * @filename: the file's name.
+ * @letters: numbers of letters it should read and print.
+ * Return: number of letters it could read and print
  *         0 if filename is NULL
- *         0 if the file can not be opned
- *         0 if write fails or does not write the expected amount of bytes.
+ *         0 if write fails.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
 	int fd;
+	char *buffer;
 	ssize_t nread, nwrite;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
-	{
 		return (0);
-	}
-	buffer = malloc((letters) * sizeof(char *));
+	buffer = malloc((letters + 1) * sizeof(char *));
 	if (buffer == NULL)
 		return (0);
 	nread = read(fd, buffer, letters);
 	nwrite = write(STDOUT_FILENO, buffer, nread);
-	close(fd);
-	free (buffer);
-	return (sum);
+	if (nwrite == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	if (nread > nwrite)
+		return (0);
+	return (nwrite);
 }
