4a99b52758cfabfec429034739d5b1d7553f48aa
tonyNTAHE <tonyntahe@gmail.com>
2023-08-10 21:44:30 +0200

    fixing errors

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,35 +9,18 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *s;
-	unsigned int min_size;
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	s = malloc(new_size);
-	if (ptr == NULL || new_size > old_size)
-	{
-		return (s);
-	}
-	if (s != NULL)
-	{
-		if (old_size > new_size)
-		{
-			min_size = old_size;
-		}
-		else
-		{
-			min_size = new_size;
-		}
-	}
-	memcpy(s, ptr, min_size);
 	free(ptr);
-	return (s);
+	ptr = malloc(new_size);
+	return (ptr);
 }
